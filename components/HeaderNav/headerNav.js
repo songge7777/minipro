a7fa6b9305bf33916ca3726f03137563d553eaff
“songge” <501798525@qq.com>
2022-09-14 14:38:24 +0800

    增加排行榜

---
+++ components/HeaderNav/headerNav.js
@@ -0,0 +1,27 @@
+// components/HeaderNav/headerNav.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name:"",
+    text:{
+      type: String,
+      value: '',
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
