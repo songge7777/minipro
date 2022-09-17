48890f0c0a2b242d6b40377ca54bbdfd25556ffa
“songge” <501798525@qq.com>
2022-09-17 15:20:22 +0800

    视频下面的分析

--- pages/video/video.js
+++ pages/video/video.js
@@ -11,6 +11,44 @@ Page({
     videoList:[],
     currentId:'',
     videoPlayHistoryData:[],//[{id,currentTime}]
+    refresher: false
+  },
+  // 上拉触底
+  async onScrolltolower(){
+    // 1、获取 videoList 的数据
+    const videoList = this.data.videoList
+    // 简单的控制一下
+    if(videoList.length < 50){
+      // 2、次函数的 调回执行的时候  拿到最新的数据
+      const r = await request('/video/group',{id:this.data.currentIndex},'GET')
+      // 3、更新视频列表
+      this.setData({
+        videoList: [...videoList, ...r.datas.map(item => item.data)] 
+      })
+    }
+    console.log('上拉触底')
+  },
+
+  // 下拉刷新的逻辑  回调函数
+  async onRefresherrefresh(){
+    // 1、下拉的时候 出现。。。。  
+    // 2、请求数据
+    await this.getVideoList()
+
+    // 3、请求成功后  refresher设置为flase
+    // setTimeout 测试延长时间
+    setTimeout(()=>{
+      this.setData({
+        refresher:false
+      })
+    },2000)
+
+    // console.log('下拉函数触发')
+    // setTimeout(()=>{
+    //   this.setData({
+    //     refresher:false
+    //   })
+    // },2000)
   },
   // seekHistoryVideo 跳转到指定的视频 上次播放的位子
   seekHistoryVideo(id){
@@ -21,6 +59,17 @@ Page({
       this.player.seek(obj.currentTime)
     }
     // 3、有 接着上一次位子播放 videoContext.seek(时间)
+  },
+  // 视频播放完成的事件 回调函数
+  getEnded(event){
+    // 拿到当前的视频 id
+    const id = event.target.id
+    const videoPlayHistoryData = this.data.videoPlayHistoryData
+    // 用数组的 filter 进行过滤 播放完成的对象
+    const obj = videoPlayHistoryData.filter(item => item.id !== id)
+    this.setData({
+      videoPlayHistoryData:obj
+    })
   },
   // 时间节点的回调函数
   getVideoTime(event){
@@ -34,7 +83,7 @@ Page({
       currentTime,
     }
     // 4、通过id  去找到历史数据 是否有 
-    const videoPlayHistoryData = this.data.videoPlayHistoryData
+    const videoPlayHistoryData = this.data.videoPlayHistoryData // []
     const obj = videoPlayHistoryData.find(item => item.id === id)
     console.log('去找到历史数据 是否有',obj)
     //  有就直接修改 没有就给他新增
@@ -79,7 +128,7 @@ Page({
     // 要先判断 当前的视频是否存在
     if(this.player){
       // 如果当前点击的id 和 上一个id 不一样 才进去
-      if(this.currentId !== id){
+      if(this.data.currentId !== id){
         // 如果有 就暂停上一个视频
         this.player.pause();
         // 创建新的 视频实例this.player赋值  在进行播放
@@ -105,11 +154,11 @@ Page({
         currentId:id
       })
     }
-    // 创建一个 video 视频实例的对象 
-    const player = wx.createVideoContext(id)
-    player.play();
-    this.player = player
-    console.log('处理点击视频操作',event)
+    // // 创建一个 video 视频实例的对象 
+    // const player = wx.createVideoContext(id)
+    // player.play();
+    // this.player = player
+    // console.log('处理点击视频操作',event)
   },
   // nav 点击 修改 currenIndex
   handleFn(e){
@@ -118,6 +167,7 @@ Page({
     })
     this.getVideoList();
   },
+  // 获取视频列表
   async getVideoList(){
     const r = await request('/video/group',{id:this.data.currentIndex},'GET')
     this.setData({
@@ -169,8 +219,15 @@ Page({
 
   /**
    * 用户点击右上角分享
+   * 1、右上角 原生分享
    */
-  onShareAppMessage() {
-
+  onShareAppMessage({from}) {
+    // from 的值 menu button用来区别原生和自定按钮
+      console.log('用户点击右上角分享',from)
+      return {
+        imageUrl:'../../static/images/mylove.jpg',
+        path:'/pages/index/index',
+        title :'测试转发'
+      }
   }
 })
