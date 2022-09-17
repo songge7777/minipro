8e755e467b4950778a4c2b0dbd45f4653e2a4b97
“songge” <501798525@qq.com>
2022-09-17 09:30:06 +0800

    视频优化

--- pages/video/video.js
+++ pages/video/video.js
@@ -9,6 +9,7 @@ Page({
     navLists:[],
     currentIndex:'',
     videoList:[],
+    currentId:'',
   },
 
   /**
@@ -31,20 +32,57 @@ Page({
     })
     console.log('获取nav 列表数据', r.data.slice(0,30))
   },
+  // clickVideo 处理点击视频操作
+  clickVideo(event){
+    // 获取当前点击 视频组件的 id
+    const id = event.target.id
+    // this.player 保证全局只有一个 播放的实例
+    // 要先判断 当前的视频是否存在
+    if(this.player){
+      // 如果当前点击的id 和 上一个id 不一样 才进去
+      if(this.currentId !== id){
+        // 如果有 就暂停上一个视频
+        this.player.pause();
+        // 创建新的 视频实例this.player赋值  在进行播放
+        // 覆盖上一次的 视频实例
+        this.player = wx.createVideoContext(id);
+        this.player.play();
+        // 保存当前播放的id
+        this.setData({
+          currentId:id
+        })
+      }else{
+        // 当前的id 和 上一次的id 一样
+      }
+    } else{
+      // 首次进入
+      // 创建新的 视频实例 给this.player赋值
+      this.player = wx.createVideoContext(id);
+      this.player.play();
+      // 保存当前播放的id
+      this.setData({
+        currentId:id
+      })
+    }
+    // 创建一个 video 视频实例的对象 
+    const player = wx.createVideoContext(id)
+    player.play();
+    this.player = player
+    console.log('处理点击视频操作',event)
+  },
   // nav 点击 修改 currenIndex
   handleFn(e){
     this.setData({
       currentIndex:e.currentTarget.dataset.id
     })
-    console.log('===>')
     this.getVideoList();
   },
   async getVideoList(){
     const r = await request('/video/group',{id:this.data.currentIndex},'GET')
     this.setData({
-      videoList: r.datas.map(item => item.data.urlInfo)
+      videoList: r.datas.map(item => item.data)
     })
-    console.log('获取视频数据',r.datas[0])
+    console.log('获取视频数据',r.datas[0].data)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
